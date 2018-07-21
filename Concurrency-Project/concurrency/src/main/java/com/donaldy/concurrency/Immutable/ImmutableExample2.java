f488d2aedd95448379b9ea7ee1047771bd764d78
DonaldY <448641125@qq.com>
2018-07-22 01:44:48 +0800

    add immutable

---
+++ Concurrency-Project/concurrency/src/main/java/com/donaldy/concurrency/Immutable/ImmutableExample2.java
@@ -0,0 +1,38 @@
+package com.donaldy.concurrency.Immutable;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by DonaldY on 2018/7/22.
+ */
+
+/**
+ * 不可修改的类
+ * Collections.unmodifiableXXX: Collection, List, Set, Map...
+ * 
+ * Guava: ImmutableXXX: Collection, List, Set, Map..
+ */
+public class ImmutableExample2 {
+    
+    private final static Integer a = 1;
+    private final static String b = "2";
+    private static Map<Integer, Integer> map = new HashMap<>();
+    
+    static {
+        map.put(1, 2);
+        map.put(3, 4);
+        map.put(5, 6);
+        map = Collections.unmodifiableMap(map);
+    }
+
+    public static void main(String[] args) {
+        map.put(1, 3);
+        System.out.println(map.get(1));
+    }
+    
+    private void test(final int a) {
+        
+    }
+}
---
+++ Concurrency-Project/concurrency/src/main/java/com/donaldy/concurrency/Immutable/ImmutableExample3.java
@@ -0,0 +1,30 @@
+package com.donaldy.concurrency.Immutable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by DonaldY on 2018/7/22.
+ */
+
+public class ImmutableExample3 {
+    
+    private final static Integer a = 1;
+    private final static String b = "2";
+    private final static Map<Integer, Integer> map = new HashMap<>();
+    
+    static {
+        map.put(1, 2);
+        map.put(3, 4);
+        map.put(5, 6);
+    }
+
+    public static void main(String[] args) {
+        map.put(1, 3);
+        System.out.println(map.get(1));
+    }
+    
+    private void test(final int a) {
+        
+    }
+}
