a9a7291c9598bf4ce1df73673fbe21a3efd59ce4
DonaldY <448641125@qq.com>
2018-09-26 09:12:35 +0800

    add comment

--- Concurrency-Project/concurrency/src/main/java/com/donaldy/concurrency/singleton/SingletonExample1.java
+++ Concurrency-Project/concurrency/src/main/java/com/donaldy/concurrency/singleton/SingletonExample1.java
@@ -3,6 +3,15 @@ package com.donaldy.concurrency.singleton;
 /**
  * Created by DonaldY on 2018/7/16.
  */
+
+/**
+ * 安全发布对象：
+ *
+ * 1. 在静态初始化函数中初始化一个对象引用
+ * 2. 将对想的引用保存到volatile类型域或者AtomicReference对象中
+ * 3. 将对象的引用保存到某个正确构造对象的final类型域中
+ * 4. 将对象的引用保存到一个由锁保护的域中
+ */
 public class SingletonExample1 {
     
     private SingletonExample1() {
