99e0a4354648babb751c0167e2625e5b749ede32
DonaldY <448641125@qq.com>
2018-09-29 09:36:14 +0800

    add threadlocal

---
+++ Concurrency-Project/concurrency/src/main/java/com/donaldy/concurrency/filter/RequestHolder.java
@@ -0,0 +1,21 @@
+package com.donaldy.concurrency.filter;
+
+/**
+ * 线程封闭
+ */
+public class RequestHolder {
+
+    private final static ThreadLocal<Long> requestHolder = new ThreadLocal<>();
+
+    public static void add(Long id) {
+        requestHolder.set(id);
+    }
+
+    public static Long getId() {
+        return requestHolder.get();
+    }
+
+    public static void remove() {
+        requestHolder.remove();
+    }
+}
