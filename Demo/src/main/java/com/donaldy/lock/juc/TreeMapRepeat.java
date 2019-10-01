93152ea7b8d6f939c95fcd7acbe7b2761f7af1c7
Donald <448641125@qq.com>
2019-10-01 16:11:11 +0800

    update treemap test

--- Demo/src/main/java/com/donaldy/lock/juc/TreeMapRepeat.java
+++ Demo/src/main/java/com/donaldy/lock/juc/TreeMapRepeat.java
@@ -13,6 +13,15 @@ public class TreeMapRepeat {
 
         // TreeMap, size = 2, 因为 Key 去重规则是根据排序结果
         System.out.println(map.size());
+
+        /*TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(55, "fifty-five");
+        treeMap.put(56, "fifty-six");
+        treeMap.put(57, "fifty-seven");
+        treeMap.put(58, "fifty-eight");
+        treeMap.put(83, "eighty-three");
+        treeMap.remove(57);
+        treeMap.put(59, "fifty-nine");*/
     }
 }
 
