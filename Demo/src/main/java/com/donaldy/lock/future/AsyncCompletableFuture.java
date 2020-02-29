353dd1f4d9bb299251e5b50d2e379cbb7adeb80c
Donald <448641125@qq.com>
2020-03-01 00:44:42 +0800

    update future async then

--- Demo/src/main/java/com/donaldy/lock/future/AsyncCompletableFuture.java
+++ Demo/src/main/java/com/donaldy/lock/future/AsyncCompletableFuture.java
@@ -58,7 +58,44 @@ public class AsyncCompletableFuture {
         System.out.println(future.get());
     }
 
+    // 异步A 完成，激活异步B
+    public static void thenRun() throws ExecutionException, InterruptedException {
 
+        CompletableFuture<String> oneFuture = CompletableFuture.supplyAsync(new Supplier<String>() {
+            @Override
+            public String get() {
+
+                try {
+
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+
+                return "hello";
+            }
+        });
+
+        // 在future上增加事件，当future计算完成后回调该事件，并返回新future
+        CompletableFuture twoFuture = oneFuture.thenRun(new Runnable() {
+
+            @Override
+            public void run() {
+
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(Thread.currentThread().getName());
+                System.out.println("---after oneFuture over doSomething---");
+            }
+        });
+
+        System.out.println(twoFuture.get());
+    }
 
     public static void main(String[] args) {
 
