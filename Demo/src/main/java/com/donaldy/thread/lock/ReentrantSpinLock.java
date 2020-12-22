3d15b441cb08d9bf50d38f8053508767681decb6
Donald <448641125@qq.com>
2020-12-22 20:18:29 +0800

    update lock demo

---
+++ Demo/src/main/java/com/donaldy/thread/lock/ReentrantSpinLock.java
@@ -0,0 +1,67 @@
+package com.donaldy.thread.lock;
+
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 描述：     实现一个可重入的自旋锁
+ *
+ * @author donald
+ * @date 2020/12/22
+ */
+public class ReentrantSpinLock  {
+
+    private AtomicReference<Thread> owner = new AtomicReference<>();
+
+    //重入次数
+    private int count = 0;
+
+    public void lock() {
+        Thread t = Thread.currentThread();
+        if (t == owner.get()) {
+            ++count;
+            return;
+        }
+        //自旋获取锁
+        while (!owner.compareAndSet(null, t)) {
+            System.out.println("自旋了");
+        }
+    }
+
+    public void unlock() {
+        Thread t = Thread.currentThread();
+        //只有持有锁的线程才能解锁
+        if (t == owner.get()) {
+            if (count > 0) {
+                --count;
+            } else {
+                //此处无需CAS操作，因为没有竞争，因为只有线程持有者才能解锁
+                owner.set(null);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ReentrantSpinLock spinLock = new ReentrantSpinLock();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "开始尝试获取自旋锁");
+                spinLock.lock();
+                try {
+                    System.out.println(Thread.currentThread().getName() + "获取到了自旋锁");
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    spinLock.unlock();
+                    System.out.println(Thread.currentThread().getName() + "释放了了自旋锁");
+                }
+            }
+        };
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        thread1.start();
+        thread2.start();
+    }
+}
