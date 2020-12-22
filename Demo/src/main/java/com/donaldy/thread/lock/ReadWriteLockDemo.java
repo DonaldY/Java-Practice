24ac67a8f8293432c7fecb5afb30c311da376a6c
Donald <448641125@qq.com>
2020-12-22 19:17:21 +0800

    update lock demo

---
+++ Demo/src/main/java/com/donaldy/thread/lock/ReadWriteLockDemo.java
@@ -0,0 +1,47 @@
+package com.donaldy.thread.lock;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author donald
+ * @date 2020/12/22
+ */
+public class ReadWriteLockDemo {
+
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock(
+            false);
+    private static final ReentrantReadWriteLock.ReadLock readLock = reentrantReadWriteLock
+            .readLock();
+    private static final ReentrantReadWriteLock.WriteLock writeLock = reentrantReadWriteLock
+            .writeLock();
+    private static void read() {
+        readLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到读锁，正在读取");
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放读锁");
+            readLock.unlock();
+        }
+    }
+    private static void write() {
+        writeLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到写锁，正在写入");
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放写锁");
+            writeLock.unlock();
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> read()).start();
+        new Thread(() -> read()).start();
+        new Thread(() -> write()).start();
+        new Thread(() -> write()).start();
+    }
+}
