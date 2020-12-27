154948d1e7170fa8eff533c32a0bb68dc6165518
Donald <448641125@qq.com>
2020-12-27 19:36:44 +0800

    add countdownlatch demo

---
+++ Demo/src/main/java/com/donaldy/lock/aqs/CountDownLatchDemo1.java
@@ -0,0 +1,37 @@
+package com.donaldy.lock.aqs;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author donald
+ * @date 2020/12/27
+ */
+public class CountDownLatchDemo1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(5);
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            final int no = i + 1;
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println(no + "号运动员完成了比赛。");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        latch.countDown();
+                    }
+                }
+            };
+            service.submit(runnable);
+        }
+        System.out.println("等待5个运动员都跑完.....");
+        latch.await();
+        System.out.println("所有人都跑完了，比赛结束。");
+    }
+}
---
+++ Demo/src/main/java/com/donaldy/lock/aqs/CountDownLatchDemo2.java
@@ -0,0 +1,36 @@
+package com.donaldy.lock.aqs;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author donald
+ * @date 2020/12/27
+ */
+public class CountDownLatchDemo2 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("运动员有5秒的准备时间");
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            final int no = i + 1;
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(no + "号运动员准备完毕，等待裁判员的发令枪");
+                    try {
+                        countDownLatch.await();
+                        System.out.println(no + "号运动员开始跑步了");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            service.submit(runnable);
+        }
+        Thread.sleep(5000);
+        System.out.println("5秒准备时间已过，发令枪响，比赛开始！");
+        countDownLatch.countDown();
+    }
+}
