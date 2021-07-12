8121a55453947ba17d8aef47a0036d3a38155e93
Donald <448641125@qq.com>
2021-07-13 00:45:21 +0800

    init filesystem FSDirectory

--- distributed-filesystem/src/main/java/com/donald/filesystem/FSDirectory.java
+++ distributed-filesystem/src/main/java/com/donald/filesystem/FSDirectory.java
@@ -1,5 +1,8 @@
 package com.donald.filesystem;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  *
@@ -9,10 +12,125 @@ package com.donald.filesystem;
 public class FSDirectory {
 
     /**
+     * 内存中的文件树
+     */
+    private INodeDirectory dirTree;
+
+    public FSDirectory() {
+        this.dirTree = new INodeDirectory("/");
+    }
+
+    /**
      * 创建目录
+     *
+     * 1. 对目录分片
+     * 2. 遍历查找是否存在对应目录
      * @param path 目录路径
      */
     public void mkdir(String path) {
 
+        String [] pathes = path.split("/");
+        INodeDirectory parent = null;
+
+
+        for (String splitedPath : pathes) {
+            if ("".equals(splitedPath.trim())) {
+                continue;
+            }
+
+            INodeDirectory dir = findDirectory(dirTree, splitedPath);
+            if (null != dir) {
+                parent = dir;
+                continue;
+            }
+            INodeDirectory child = new INodeDirectory(splitedPath);
+            parent.addChild(child);
+        }
+    }
+
+    /**
+     * 对文件目录树递归查找目录
+     * @param dir 目录
+     * @param path 路径
+     * @return 目录
+     */
+    private INodeDirectory findDirectory(INodeDirectory dir, String path)  {
+        if (dir.getChildren().size() == 0) {
+            return null;
+        }
+
+        INodeDirectory resultDir = null;
+
+        for (INode child : dir.getChildren()) {
+            if (child instanceof INodeDirectory) {
+                INodeDirectory childDir = (INodeDirectory) child;
+
+                if (childDir.getPath().equals(path)) {
+                    return childDir;
+                }
+                resultDir = findDirectory(childDir, path);
+
+                if (resultDir != null) {
+                    return resultDir;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 文件目录树中的一个节点
+     */
+    public interface INode {
+
+    }
+
+    /**
+     * 文件目录树中的一个目录
+     */
+    public static class INodeDirectory implements INode {
+        private String path;
+        private List<INode> children;
+
+        public INodeDirectory(String path) {
+            this.path = path;
+            this.children = new LinkedList<>();
+        }
+
+        public void addChild(INode node) {
+            this.children.add(node);
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
+        public List<INode> getChildren() {
+            return children;
+        }
+
+        public void setChildren(List<INode> children) {
+            this.children = children;
+        }
+    }
+
+    /**
+     * 文件目录树中一个文件
+     */
+    public static class INodeFile implements INode {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
